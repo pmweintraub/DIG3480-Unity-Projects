c9fefc9c23a4b1e9736f63e50f3b933d3cfce1e1
pmweintraub <pmweintraub@knights.ucf.edu>
2019-10-26 00:59:49 -0400

    Challenge 2 for DIG3480

---
+++ Platformer and Tilemapping Challenge/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public float speed;
+    public Text winText;
+    public Text scoreText;
+    public Text livesText;
+    public GameObject player;
+
+    private int lives;
+
+    private Rigidbody2D rd2d;
+    private bool facingRight = true;
+    private int score;
+    private int hozMovement;
+
+    Animator anim;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        hozMovement = 0;
+        anim = GetComponent<Animator>();
+        winText.text = "";
+        score = 0;
+        lives = 3;
+        SetScoreText();
+        SetLivesText();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+       
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetScoreText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+        if (lives == 0)
+        {
+            Destroy(player);
+        }
+    }
+
+
+
+
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+            {
+                anim.SetInteger("State", 0);
+            }
+        }
+    }
+
+
+
+    void SetScoreText()
+    {
+        if (score == 4)
+        {
+            player.transform.position = new Vector2(-84f, 1f);
+            lives = 3;
+            SetLivesText();
+        }
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 8)
+        {
+            winText.text = "You win! Game created by Paulina Weintraub!";
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            
+        }
+    }
+
+ 
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            winText.text = "You lose! Game created by Paulina Weintraub!";
+        }
+    }
+}
