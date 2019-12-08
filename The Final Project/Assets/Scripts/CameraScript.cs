b683c96594130bc8aa1a646cce823eb8f1d59b7c
pmweintraub <pmweintraub@knights.ucf.edu>
2019-12-08 18:06:09 -0500

    Final Project for DIG3480

---
+++ The Final Project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public AudioSource mSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
+    private Vector3 offset;
+
+    private static int playerScore;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        mSource.clip = musicClipOne;
+        mSource.Play();
+        
+    }
+
+        void Update()
+    {
+
+        playerScore = PlayerScript.score;
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+
+        if (playerScore >= 8)
+        {
+            if (mSource.clip == musicClipOne)
+            {
+
+                mSource.clip = musicClipTwo;
+
+                mSource.Play();
+
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ The Final Project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+
+    public float speed = 1.0F;
+
+    private float startTime;
+    private float journeyLength;
+    private bool faceRight = true;
+    private Rigidbody2D rd2d;
+
+
+
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        startTime = Time.time;
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+
+    }
+
+    void Flip()
+    {
+        faceRight = !faceRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
+
+        /**/
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("EndPos"))
+        {
+            faceRight = false;
+        }
+        if (other.gameObject.CompareTag("StartPos"))
+        {
+            faceRight = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(faceRight == false)
+        {
+            Flip();
+        }
+    }
+
+}
